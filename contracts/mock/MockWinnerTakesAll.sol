2ef3ddd2559d8546cb768bec4caf7985da78e76f
Theo Richard <theo.richard.contact@gmail.com>
2022-01-22 05:26:34 +0100

    file renaming & move mock contract in mock/*

--- contracts/mock/MockWinnerTakesAll.sol
+++ contracts/mock/MockWinnerTakesAll.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "./SolidityHackingWorkshopV8.sol";
+import "../SolidityHackingWorkshopV8.sol";
 
 contract MockWinnerTakesAll is WinnerTakesAll {
   function getRoundLength() external view returns (uint256) {
